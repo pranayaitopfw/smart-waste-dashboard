e853b42e151a84c5feaaf91e8ea4b4cd7b92fbf8
PRANAY DORATE <aitopfw@gmail.com>
2026-06-03 18:52:58 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,29 @@
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 
+import { getDatabase }
+from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
+
+const firebaseConfig = {
+
+apiKey: "YOUR_API_KEY",
+
+authDomain: "YOUR_PROJECT.firebaseapp.com",
+
+databaseURL:
+"https://YOUR_PROJECT-default-rtdb.firebaseio.com",
+
+projectId: "YOUR_PROJECT",
+
+storageBucket:
+"YOUR_PROJECT.appspot.com",
+
+messagingSenderId: "XXXX",
+
+appId: "XXXX"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
